5720e3472a90f04642c1e6f240e99f17d65afcae
Daniel Correa <daniel.correa92@hotmail.com>
2022-03-21 01:33:13 -0600

    Creamos Layout principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import { useState } from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import IniciarSession from './layout/IniciarSession'
 import Layout from './layout/Layout'
 import Inicio from './paginas/Inicio'
-import LoginForm from './paginas/LoginForm'
+import NuevoCliente from './paginas/NuevoCliente'
+import EditarCliente from './paginas/EditarCliente'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-
-        <Route path="/" element={<IniciarSession />}>
-          <Route index element={<LoginForm />} />
-        </Route>
-
         <Route path='/clientes' element={<Layout />}>
           <Route index element={<Inicio />}/>
+          <Route path='nuevo' element={<NuevoCliente />}/>
+          <Route path='editar/:id' element={<EditarCliente />}/>
         </Route>
       </Routes>
     </BrowserRouter>
