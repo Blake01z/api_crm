77553841955e14c4707a4653447e8e89c3d0d87e
Daniel Correa <daniel.correa92@hotmail.com>
2022-03-21 16:40:46 -0600

    Eliminar Cliente agregado

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -19,6 +19,24 @@ const Inicio = () => {
     obtenerClientesAPI()
   },[])
 
+  const handleEliminar = async (id) => {
+    const confirmar = confirm('¿Deseas Eliminar Este Cliente?')
+
+    if(confirmar){
+      try {
+        const url = `http://localhost:4000/clientes/${id}`
+        const respuesta = await fetch(url,{
+          method: 'DELETE'
+        })
+        await respuesta.json()
+        const arrayClientes = clientes.filter(cliente => cliente.id !== id)
+        setClientes(arrayClientes)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
   return (
     <div>
       <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
@@ -36,7 +54,7 @@ const Inicio = () => {
 
         <tbody>
             {clientes.map(cliente => (
-              <Cliente key={cliente.id} cliente={cliente}/>
+              <Cliente key={cliente.id} cliente={cliente} handleEliminar={handleEliminar}/>
             ))}
         </tbody>
       </table>
