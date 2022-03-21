39bb7cbc53bb68c3f2264d178d0018b063fae0ce
Daniel Correa <daniel.correa92@hotmail.com>
2022-03-21 15:37:27 -0600

    Solucion si no hay registro

--- src/paginas/VerCliente.jsx
+++ src/paginas/VerCliente.jsx
@@ -25,40 +25,43 @@ const VerCliente = () => {
     },[])
 
   return (
-    <div>
-        {cargnado ? 'Cargandooo..' : (
 
-        <>
-            <h1 className='font-black text-4xl text-blue-900'>Ver Cliente: {cliente.nombre}</h1>
-            <p className='mt-3'>Informacion Detallada del Cliente</p>
+    Object.keys(cliente).length === 0 ? <p>No hay Resultados</p> : (
+        <div>
+            {cargnado ? 'Cargandooo..' : (
 
-            <p className="text-4xl text-gray-600 mt-10">
-            <span className="text-gray-800 uppercase font-bold">Cliente: </span> 
-            {cliente.nombre}
-            </p>
-            <p className="text-2xl mt-4 text-gray-600">
-            <span className="text-gray-800 uppercase font-bold">Email: </span> 
-            {cliente.email}
-            </p>
-            {cliente.telefono && (
-                <p className="text-2xl mt-4 text-gray-600">
-                    <span className="text-gray-800 uppercase font-bold">Telefono: </span> 
-                    {cliente.telefono}
+            <>
+                <h1 className='font-black text-4xl text-blue-900'>Ver Cliente: {cliente.nombre}</h1>
+                <p className='mt-3'>Informacion Detallada del Cliente</p>
+
+                <p className="text-4xl text-gray-600 mt-10">
+                <span className="text-gray-800 uppercase font-bold">Cliente: </span> 
+                {cliente.nombre}
                 </p>
-            )}
-            <p className="text-2xl mt-4 text-gray-600">
-            <span className="text-gray-800 uppercase font-bold">Empresa: </span> 
-            {cliente.empresa}
-            </p>
-            {cliente.notas && (
                 <p className="text-2xl mt-4 text-gray-600">
-                    <span className="text-gray-800 uppercase font-bold">Notas: </span> 
-                    {cliente.notas}
+                <span className="text-gray-800 uppercase font-bold">Email: </span> 
+                {cliente.email}
                 </p>
+                {cliente.telefono && (
+                    <p className="text-2xl mt-4 text-gray-600">
+                        <span className="text-gray-800 uppercase font-bold">Telefono: </span> 
+                        {cliente.telefono}
+                    </p>
+                )}
+                <p className="text-2xl mt-4 text-gray-600">
+                <span className="text-gray-800 uppercase font-bold">Empresa: </span> 
+                {cliente.empresa}
+                </p>
+                {cliente.notas && (
+                    <p className="text-2xl mt-4 text-gray-600">
+                        <span className="text-gray-800 uppercase font-bold">Notas: </span> 
+                        {cliente.notas}
+                    </p>
+                )}
+            </>
             )}
-        </>
-        )}
-    </div>
+        </div>
+    )
   )
 }
 
